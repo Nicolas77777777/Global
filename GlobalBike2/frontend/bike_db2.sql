9156b630ead0dad4f5dad33dc89c2ef0b6f5703f
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2025-05-26 15:16:41 +0200

    last commit MAC

---
+++ GlobalBike2/frontend/bike_db2.sql
@@ -0,0 +1,26 @@
+-- Tabelle db bike -- 
+
+CREATE TABLE login (
+    id_login SERIAL PRIMARY KEY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE cliente (
+    id_cliente SERIAL PRIMARY KEY,
+    numero_tessera VARCHAR(50) UNIQUE,
+    cellulare VARCHAR(20),
+    nome VARCHAR(100),
+    cognome_rag_soc VARCHAR(150) NOT NULL,
+    luogo_nascita VARCHAR(100),
+    data_nascita DATE NOT NULL,
+    data_iscrizione DATE NOT NULL,
+    data_scadenza DATE NOT NULL,
+    indirizzo VARCHAR(200),
+    citta VARCHAR(100),
+    provincia VARCHAR(50),
+    cap VARCHAR(20),
+    cf_piva VARCHAR(50),
+    email VARCHAR(150) NOT NULL,
+    note TEXT
+);
