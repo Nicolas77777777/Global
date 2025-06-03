dcef7c018843ec67b4301533b7ad99fb05868999
Nicolas77777777 <sairwater@gmail.com>
2025-06-03 13:43:10 +0200

    second commit HP

--- GlobalBike2/frontend/index.js
+++ GlobalBike2/frontend/index.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = express();
-const port = 8081;
+const port = 8082;
 
 // Imposta EJS come motore di visualizzazione
 server.set('view engine', 'ejs');
